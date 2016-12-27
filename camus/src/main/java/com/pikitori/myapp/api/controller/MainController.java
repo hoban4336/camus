b0ff0115d6595151052a0dce9529873213666e60
yeon <yeon@221.167.211.109>
2016-12-28 01:11:42 +0900

    === 1 ===

---
+++ camus/src/main/java/com/pikitori/myapp/api/controller/MainController.java
@@ -0,0 +1,40 @@
+package com.pikitori.myapp.api.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.pikitori.myapp.service.UserService;
+import com.pikitori.myapp.vo.User;
+
+@Controller
+@RequestMapping("/user")
+public class MainController {
+
+	@RequestMapping("")
+	@ResponseBody
+	public String index(){
+		return "index";
+	}
+	
+	@Autowired
+	UserService userService;
+	
+	@RequestMapping( "/list" )
+	@ResponseBody
+	public Object checkEmail() {
+		
+		List<User> list = userService.getAllUsers();
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put( "result", "success" );
+		map.put( "data", list );
+		
+		return map;
+	}
+}
